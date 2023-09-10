f4494b26389292c4b96756a60bc299e7cc7a0723
edilton-bs <ediltonbs5@gmail.com>
2023-09-09 21:30:44 -0300

    com dados csv

--- main.py
+++ main.py
@@ -6,6 +6,15 @@ import base64
 st.title('Criar/editar metas 📝')
 st.write("Insira suas metas e monitore seu progresso!")
 
+csv_file = "metas_data.csv"
+
+# Ler os dados do CSV
+try:
+    data = pd.read_csv(csv_file)
+except FileNotFoundError:
+    data = pd.DataFrame(columns=["NOME", "META", "VALOR"])
+    data.to_csv(csv_file, index=False)
+
 # Função para download do dataframe
 def get_table_download_link(df, filename):
     csv = df.to_csv(index=False)
@@ -13,79 +22,44 @@ def get_table_download_link(df, filename):
     href = f'<a href="data:file/csv;base64,{b64}" download="{filename}.csv">Download CSV</a>'
     return href
 
-# Se o dataframe ainda não existe, vamos criá-lo
-if 'data' not in st.session_state:
-    st.session_state.data = pd.DataFrame(columns=["NOME", "META", "VALOR"])
-
 # Formulário para adicionar metas
 with st.form(key="add_goal"):
     name = st.text_input("Nome")
     meta_name = st.number_input("Meta (Soma total de valores que quero atingir)", value=0)
-    # meta_value = st.number_input("Valor", value=0.0)
     submit_button = st.form_submit_button("Adicionar/Atualizar Valor")
 
-    # Quando o formulário for enviado, adiciona a meta ao dataframe
     if submit_button:
-        st.session_state.data.loc[len(st.session_state.data)] = [name, meta_name, 0]
+        data.loc[len(data)] = [name, meta_name, 0]
+        data.to_csv(csv_file, index=False)
         st.success(f"Valor para {name} adicionado com sucesso!")
-
-        # Verifica se a meta foi atingida
-        total_value = st.session_state.data[st.session_state.data["NOME"] == name]['VALOR'].sum()
-        if total_value >= meta_name:
-            st.success(f"Parabéns, {name}! Você conseguiu!")
-
-
-# Edição e Exclusão de Entradas
-# ... [código anterior]
-
-# Edição e Exclusão de Entradas
-# ...
 
 # Edição e Exclusão de Entradas
 with st.expander("Editar ou Excluir Metas"):
-    if not st.session_state.data.empty:  # Verifique se o dataframe não está vazio
-        # Selecione o índice da meta
-        index_to_edit_or_delete = st.selectbox("Selecione o índice da meta para editar ou excluir", st.session_state.data.index)
-
-        # Seção de edição
-        new_name = st.text_input("Editar Nome", st.session_state.data.at[index_to_edit_or_delete, 'NOME'])
-        new_meta = st.number_input("Editar Meta", value=st.session_state.data.at[index_to_edit_or_delete, 'META'])
-        new_value = st.number_input("Editar Valor", value=st.session_state.data.at[index_to_edit_or_delete, 'VALOR'])
+    if not data.empty:  # Verifique se o dataframe não está vazio
+        index_to_edit_or_delete = st.selectbox("Selecione o índice da meta para editar ou excluir", data.index)
+        new_name = st.text_input("Editar Nome", data.at[index_to_edit_or_delete, 'NOME'])
+        new_meta = st.number_input("Editar Meta", value=data.at[index_to_edit_or_delete, 'META'])
+        new_value = st.number_input("Editar Valor", value=data.at[index_to_edit_or_delete, 'VALOR'])
 
         with st.form(key="edit_delete_form"):
             form_submit_button = st.form_submit_button("Salvar Alterações")
-
-            # Atualizar os valores se o botão for pressionado
             if form_submit_button:
-                st.session_state.data.at[index_to_edit_or_delete, 'NOME'] = new_name
-                st.session_state.data.at[index_to_edit_or_delete, 'META'] = new_meta
-                st.session_state.data.at[index_to_edit_or_delete, 'VALOR'] = new_value
+                data.at[index_to_edit_or_delete, 'NOME'] = new_name
+                data.at[index_to_edit_or_delete, 'META'] = new_meta
+                data.at[index_to_edit_or_delete, 'VALOR'] = new_value
                 st.success("Meta atualizada com sucesso!")
-        
-        # Seção de exclusão (movido para fora do formulário)
+                data.to_csv(csv_file, index=False)
+
         if st.button("Excluir Meta", key='delete'):
-            st.session_state.data = st.session_state.data.drop(index_to_edit_or_delete)  # Remove a linha com base no índice
+            data = data.drop(index_to_edit_or_delete)
+            data.to_csv(csv_file, index=False)
             st.success("Meta excluída com sucesso!")
     else:
         st.write("Nenhuma meta disponível para edição ou exclusão.")
 
-# ...
-
-
-
-# ... [código posterior]
-
-
-
-
 # Exibe o dataframe
 st.write("Seus dados até o momento:")
-st.write(st.session_state.data)
+st.write(data)
 
 # Download do dataframe
-st.markdown(get_table_download_link(st.session_state.data, "metas_data"), unsafe_allow_html=True)
-
-# Gráfico de linha
-# st.write("Progresso das Metas:")
-# for name in st.session_state.data["NOME"].unique():
-#    st.line_chart(st.session_state.data[st.session_state.data["NOME"] == name][["VALOR"]].cumsum(), use_container_width=True)
+st.markdown(get_table_download_link(data, "metas_data"), unsafe_allow_html=True)
--- pages/progresso.py
+++ pages/progresso.py
@@ -9,52 +9,49 @@ from spotipy.oauth2 import SpotifyClientCredentials
 sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id="2173f16a9c7c4c90bc3ebecbfed52a15",
                                                            client_secret="2685aa5dbd0b40de870759851e02f58a"))
 
-
 # Configurações iniciais do Streamlit
 st.title('📈 Meu progresso')
 
-# Função para download do dataframe
-def get_table_download_link(df, filename):
-    csv = df.to_csv(index=False)
-    b64 = base64.b64encode(csv.encode()).decode()
-    href = f'<a href="data:file/csv;base64,{b64}" download="{filename}.csv">Download CSV</a>'
-    return href
+csv_file = "metas_data.csv"
 
-# Se o dataframe ainda não existe, vamos criá-lo
-if 'data' not in st.session_state:
-    st.session_state.data = pd.DataFrame(columns=["NOME", "META", "VALOR"])
+# Ler os dados do CSV
+try:
+    data = pd.read_csv(csv_file)
+except FileNotFoundError:
+    data = pd.DataFrame(columns=["NOME", "META", "VALOR"])
+    data.to_csv(csv_file, index=False)
 
-# Formulário para adicionar metas
+# Seção para adicionar metas está aqui
 with st.expander("Add dados"):
     with st.form(key="add_goal"):
-        
-        # Se há nomes no dataframe, usa eles como opções, senão, usa uma lista vazia
-        available_names = st.session_state.data["NOME"].unique().tolist() if not st.session_state.data.empty else []
-        
+        available_names = data["NOME"].unique().tolist() if not data.empty else []
         name = st.selectbox("Nome", options=available_names)
         meta_value = st.number_input("Valor", value=0)
         submit_button = st.form_submit_button("Adicionar/Atualizar Valor")
 
-        # Quando o formulário for enviado, adiciona a meta ao dataframe
-        if submit_button and not st.session_state.data.empty:
-            meta_name = st.session_state.data[st.session_state.data["NOME"] == name]['META'].iloc[0]
-            st.session_state.data.loc[len(st.session_state.data)] = [name, meta_name, meta_value]
+        if submit_button and not data.empty:
+            meta_name = data[data["NOME"] == name]['META'].iloc[0]
+            data.loc[len(data)] = [name, meta_name, meta_value]
+            data.to_csv(csv_file, index=False)
             st.success(f"Valor para {name} adicionado com sucesso!")
 
 # Verifica se a meta foi atingida e plota gráficos fora do bloco 'if submit_button'
-if not st.session_state.data.empty:
-    total_value = st.session_state.data[st.session_state.data["NOME"] == name]['VALOR'].sum()
-    meta_name = st.session_state.data[st.session_state.data["NOME"] == name]['META'].iloc[0]
+if not data.empty:
+    total_value = data[data["NOME"] == name]['VALOR'].sum()
+    meta_name = data[data["NOME"] == name]['META'].iloc[0]
+    
     if total_value >= meta_name:
         st.success(f"Parabéns, {name}! Você conseguiu!")
 
         # Busca faixas baseadas em um critério
         tracks = sp.search(q='genre:"Indie"', type='track', limit=50)
-
+        
         # Verifica se temos faixas na resposta
         if tracks['tracks']['items']:
             # Escolhe uma faixa aleatória
+            
             random_track = random.choice(tracks['tracks']['items'])
+            
             
             # Exibe informações sobre a faixa
             track_name = random_track['name']
@@ -64,14 +61,14 @@ if not st.session_state.data.empty:
             st.success(f"Sua recompensa é ouvir a música: [{track_name} de {track_artist}]({track_link})")
         else:
             st.warning("Não foi possível encontrar uma faixa. Tente novamente mais tarde.")
-
+    
 
 
 
     # Gráfico de linha não-acumulativo com pontos
     st.write("Progresso:")
     #for name in st.session_state.data["NOME"].unique():
-    df = st.session_state.data[st.session_state.data["NOME"] == name][["VALOR"]]
+    df = data[data["NOME"] == name][["VALOR"]]
     plt.figure(figsize=(10, 4))
     plt.plot(df["VALOR"], marker='o')
     plt.title(f"Progresso de {name}")
@@ -81,7 +78,7 @@ if not st.session_state.data.empty:
     
     # Gráfico da meta escolhida usando Matplotlib
     st.write(f"Progresso da Meta: {name}")
-    df = st.session_state.data[st.session_state.data["NOME"] == name][["VALOR"]].cumsum()
+    df = data[data["NOME"] == name][["VALOR"]].cumsum()
     plt.figure(figsize=(10, 4))
     plt.plot(df["VALOR"], marker='o')
     plt.title(f"Progresso Cumulativo da Meta: {name}")
